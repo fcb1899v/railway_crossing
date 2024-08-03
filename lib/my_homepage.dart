f4e27307a7a47de0143868ee2e51917d9287fa0f
nakajimamasao <chelseav3@hotmail.com>
2024-08-03 11:32:49 +0900

    1st Release 0.0.1

--- lib/my_homepage.dart
+++ lib/my_homepage.dart
@@ -5,7 +5,6 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:devicelocale/devicelocale.dart';
 import 'package:fab_circular_menu_plus/fab_circular_menu_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:vibration/vibration.dart';
