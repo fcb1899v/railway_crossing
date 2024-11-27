8cc9d29aafb619c1a97fee6e07a8baae6259f454
nakajimamasao <chelseav3@hotmail.com>
2024-11-28 07:40:07 +0900

    Ver. 1.1.0 Add AI photo and Subscription Plan.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -7,20 +8,34 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'common_widget.dart';
 import 'constant.dart';
 import 'firebase_options.dart';
 import 'my_homepage.dart';
 
+const isDebugMode = false;
+const defaultIsShowAd = !isDebugMode;
+String defaultPlan = isDebugMode ? premiumID: freeID;
+const defaultTickets = isDebugMode ? 72: 0;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await MobileAds.instance.initialize();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await dotenv.load(fileName: 'assets/.env');
+  await Purchases.setLogLevel(LogLevel.debug);
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]); //横向き指定
-  runApp(const ProviderScope(child: MyApp()));
+  MobileAds.instance.initialize();
+  await dotenv.load(fileName: "assets/.env");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: androidProvider,
+    appleProvider: appleProvider,
+  );
+  await initPurchase();
+  runApp(const ProviderScope(child: MyApp())
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -31,7 +46,7 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       title: 'LETS CROSSING',
-      theme: ThemeData(colorScheme: const ColorScheme.light(primary: greenColor)),
+      theme: ThemeData(colorScheme: const ColorScheme.light(primary: redColor)),
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
       routes: {'/h' : (_) => MyHomePage()},
